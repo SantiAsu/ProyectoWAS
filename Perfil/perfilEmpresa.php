e4d9c22906bd04eea171a7e53934f49aa8573b3e
SantiAsu <santifuentest12@gmail.com>
2025-06-24 01:36:57 -0600

    Implementacion de Perfil empresa y Usuario
    Detalle, al momento de acceder al perfil debe detectar si es usuario o empresa, para dar la vista adecuada

---
+++ Perfil/perfilEmpresa.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Perfil de Usuario</title>
+  <link rel="stylesheet" href="stylePerfil.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar">
+      <div class="logo">WebAppSecure</div>
+      <ul class="nav-links">
+            <li><a href="../index.html">Inicio</a></li>
+            <li><a href="#">Buscar Empleo</a></li>
+            <li><a href="perfil.php">Perfil</a></li>
+            <li><a href="../inicioSesion/inicio.php">Iniciar Sesión</a></li>
+            <li><a href="../registroUsuarios/registro.html">Registro</a></li>
+
+      </ul>
+    </nav>
+  </header>
+
+  <div class="perfil-container">
+    <div class="perfil-img"></div>
+    <div class="perfil-nombre">
+      Nombre Empresa
+    </div>
+    <div class="perfil-grid">
+      <div class="perfil-presentacion">
+        <h4>Presentación</h4>
+        <p>Texto de Presentación de la empresa</p>
+      </div>
+      <div class="perfil-contacto">
+        <h4>Contacto</h4>
+        <p>Teléfono<br>Gmail<br>Sitioweb</p>
+
+      </div>
+      <div class="perfil-experiencia">
+        <h4>Informacion</h4>
+        <p>Pais<br>RFC</p>
+      </div>
+    </div>
+  </div>
+
+  <footer class="footer">
+    <div class="footer-content">
+      <div class="footer-logo">
+        WebAppSecure
+      </div>
+      <div class="footer-links">
+        <a href="#">Agrega un comentario</a>
+        <a href="#">Contactos</a>
+      </div>
+      <div class="footer-copy">
+        &copy; 2025 WebAppSecure.
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
